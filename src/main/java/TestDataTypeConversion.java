bdec91ccd67e133c86c2628fba1c59046ab1cfa5
lixixi89055465 <1850094299@qq.com>
2022-12-12 17:19:55 +0800

    init

--- src/main/java/TestDataTypeConversion.java
+++ src/main/java/TestDataTypeConversion.java
@@ -1,29 +1,11 @@
 public class TestDataTypeConversion {
     public static void main(String[] args) {
-        byte b = 10;
-        test(b);
-        char c = 'a';
-        test(c);
+        int x=5;
+        x=x++;
+        System.out.println(x);
+        x=++x;
+        System.out.println(x);
 
-    }
-
-//    private static void test(byte b) {
-//        System.out.println("bbbbbbb");
-//    }
-
-//    private static void test(short s) {
-//        System.out.println("ssssss");
-//    }
-
-//    private static void test(char c) {
-//        System.out.println("cccccccc");
-//    }
-
-//    private static void test(int i) {
-//        System.out.println("iiiiiiiiii");
-//    }
-
-    private static void test(long i) {
-        System.out.println("LLLLLLLLLLLL");
     }
 }
+
--- src/main/scala/chapter02/Test08_DataTypeConversion.scala
+++ src/main/scala/chapter02/Test08_DataTypeConversion.scala
@@ -2,31 +2,56 @@ package chapter02
 
 object Test08_DataTypeConversion {
   def main(args: Array[String]): Unit = {
-    var a1: Byte = 10
-    val b1: Long = 2353L
-    val result1: Int = (a1 + b1).toInt
-    println(result1)
-    val a2: Byte = 10
-    val b2: Int = a2
-    val c2: Byte = b2.toByte
-    println(c2)
-    val a3: Byte = 10
-    val b3: Int = a2
-    val c3: Byte = b3.toByte
-    println(c3)
-    println("4" * 100)
-    val a4: Byte = 12
-    val b4: Short = 25
-    val c4: Char = 'C'
-    var result4: Int = (a4 + b4)
-    println(result4)
+    for (i <- 0 to 10) {
+      //      println(i)
+    }
+    for (i: Int <- 1.to(10)) {
+      //      print(i)
+    }
+    for (i <- Range(1, 10)) {
+      print(i + ",hello world")
+    }
+    println()
+    for (i <- 1 until 10) {
+      print(i + ", until")
+    }
+    println()
+    for (i <- Array(12, 34, 53)) {
+      print(i + ",")
+    }
+    println()
+    for (i <- List(12, 34, 53)) {
+      print(i + ",")
+    }
+    println()
+    for (i <- Set(12, 34, 53)) {
+      print(i + ",")
+    }
+    println()
+    for (i <- 1 to 3 if i != 2) {
+      print(i + "\t")
+    }
 
-    println("5" * 100)
-    val m: Int = "12".toInt
-    val f: Float = "12.3".toFloat
-    val f2: Int = "12.4".toFloat.toInt
-    println(m)
-    println(f)
-    println(f2)
+    println()
+    for (i <- 1 to 10 by 2) {
+      print(i + "\t")
+    }
+    println()
+    for (i <- 0 to 30 by 3) {
+      print(i + "\t")
+    }
+    println("1" * 100)
+    for (i <- 1 to 10 reverse) {
+      print(i)
+    }
+    println("2" * 100)
+    for (data <- 1.0 to 10.0 by 0.3) {
+      print(data)
+    }
+    println()
+    for (i <- 1 to 4; j <- 1 to 5) {
+      print(s"$i * $j = ${i * j} \t ")
+    }
+
   }
 }
